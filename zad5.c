2ded98499039e0417f9208a56728540fd20af20d
glasnov89 <118739554+glasnov89@users.noreply.github.com>
2022-11-28 13:33:29 +0200

    Add files via upload

--- zad5.c
+++ zad5.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
-int main(){
-int a , b , c ;
-    printf("a = " , a);
-	scanf("%d" , &a);
-	printf("b = " , b);
-	scanf("%d" , &b);
-	printf("c = " , c);
-	scanf("%d" , &c);
-	if (a || b || c){
-	printf("Lamp is on!\n");
-	} else {
-	printf("Lamp is off!\n");
-	}
-	return 0;
-	}
+int main() {
+
+int a,b,c,d,e;
+
+printf("Vuvedi a:");
+scanf("%d",&a);
+printf("Vuvedi b:");
+scanf("%d",&b);
+printf("Vuvedi c:");
+scanf("%d",&c);
+printf("Vuvedi d:");
+scanf("%d",&d);
+printf("Vuvedi e:");
+scanf("%d",&e);
+
+if(((a&&b) || (c&&d))&&e)
+{
+    printf("Lamp is ON!\n");
+}
+else 
+{
+    printf("Lamp is OFF!\n");
+}
+    return 0;
+}
